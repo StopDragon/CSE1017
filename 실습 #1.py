8c5eea52bbf294d04a5720a12ba0f788c204cda1
JUNG JI YONG <jiyong210@gmail.com>
2019-03-17 22:23:29 +0900

    First Commit

---
+++ 실습 #1.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+print('해와 달 수를 날짜 수로 환산해드립니다.')
+year = int(input('몇 년?'))
+month = int(input('몇 개월?'))
+answer = (year * 365) + (month * 30)
+print(year, '년', month, '개월은 날짜로 환산하면',int(answer), '일입니다.')
---
+++ 실습 #2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+print('동전 합산 해줄께... 음수는 넣지마.')
+FH = int(input('오백원짜리 몇개?'))
+H = int(input('백원짜리 몇개?'))
+FT = int(input('오십원짜리 몇개?'))
+T = int(input('십원짜리 몇개?'))
+answer = FH * 500 + H * 100 + FT * 50 + T * 10
+print('자기가 갖고 있는 동전은 총', answer,'원 이야.')
