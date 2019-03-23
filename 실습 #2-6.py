08c1a9eaec6111ffca5c69ef221f2fa4cfc3a595
JUNG JI YONG <jiyong210@gmail.com>
2019-03-23 21:54:23 +0900

    ver 1

--- 실습 #2-6.py
+++ 실습 #2-6.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 def isfloat(s):
-    P = s.partition('.')
-    if '.' > P[0] >= '-':
-        return P[2].isdigit() or P[2] == ''
-    elif P[0].isdigit():
-        return P[2].isdigit() or P[2] == ''
-    elif P[0] == '':
-        return P[2].isdigit()
-    else:
+    if s >= 0 and (s.partition('.')[2]).isdigit():
+        return True
+    elif s[0] == '-' and s[1].isdigit() or (s.partition('.')[2]).isdigit():
+        return True
+    elif s[0].isdigit():
+        return True
+    else :
         return False
 
 print(isfloat(".112"))#True
