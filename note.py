c716a9a1ae13cb09b829bb4326e5fdfb037fb4ad
JUNG JI YONG <jiyong210@gmail.com>
2019-04-25 21:25:22 +0900

    sync

--- note.py
+++ note.py
@@ -1,7 +1,7 @@
 def count_upto(n):
     ns = []
-    while n > 0:
-        n = n - 1
+    while n >= 0:
         ns = [n] + ns
+        n = n - 1
     return ns
 print(count_upto(5)) # [0, 1, 2, 3, 4, 5]
