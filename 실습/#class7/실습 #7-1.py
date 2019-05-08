2d44687be584544ddb21be75c0cbff5a5184f668
JUNG JI YONG <jiyong210@gmail.com>
2019-05-08 15:54:56 +0900

    Update 실습 #7-1.py

---
+++ 실습/#class7/실습 #7-1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import random
+def create_board():
+    seed = [1,2,3,4]
+    random.shuffle(seed)
+    pass # 여기에 적절한 코드 작성
+    return None # None를 적절한 식으로 대치
+
+print(create_board())
