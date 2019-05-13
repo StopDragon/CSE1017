60193aee366c49603bace4389957080788a32d99
stopdragon <jiyong210@gmail.com>
2019-05-13 20:28:37 +0900

    class7

--- 실습/#class8/실습 #8-1.py
+++ 실습/#class8/실습 #8-1.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+import random
+def fresh_deck():
+    suits = {"Spade", "Heart", "Diamond", "Club"}
+    ranks = {"A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"}
+    deck = []
+    # 중첩 for 문으로 52장의 카드를 만들어 리스트 deck을 만든다.
+    for s in suits:
+        for r in ranks:
+            deck = deck + [{'suit': s,'rank': r}]
+    # deck을 무작위로 섞는다.
+    random.shuffle(deck)
+    return deck
---
+++ 실습/#class8/실습 #8-2.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+def hit(deck):
+    if # deck이 비어있으면:
+        # 카드 1벌을 새로 만듬
+    return # (맨 앞의 카드 한장 , 남은 deck)
